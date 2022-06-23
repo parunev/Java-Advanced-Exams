151c510c04f57d3e8099c1912b24d71eba4da3e1
Martin Parunev <parunev@gmail.com>
2022-06-23 16:57:40 +0300

    solved problem

---
+++ src/E10RetakeExam19august2020/vetClinic/Main.java
@@ -0,0 +1,32 @@
+package E10RetakeExam19august2020.vetClinic;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Clinic clinic = new Clinic(20);
+        Pet dog = new Pet("Ellias",5,"Tim");
+
+        System.out.println(dog);
+
+        clinic.add(dog);
+
+        System.out.println(clinic.remove("Ellias"));
+        System.out.println(clinic.remove("Pufa"));
+
+        Pet cat = new Pet("Bella",2,"Mia");
+        Pet bunny = new Pet("Zak",4,"Jon");
+
+        clinic.add(cat);
+        clinic.add(bunny);
+
+        Pet getOldestPet = clinic.getOldestPet();
+        System.out.println(getOldestPet);
+
+        Pet pet = clinic.getPet("Bella","Mia");
+        System.out.println(pet);
+
+        System.out.println(clinic.getCount());
+
+        System.out.println(clinic.getStatistics());
+    }
+}
